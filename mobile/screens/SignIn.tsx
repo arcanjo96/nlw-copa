827462967d21109de67685952c0d50ac807f4347
Lucas Arcanjo <lucasarcanjo.ld@gmail.com>
2022-11-02 15:59:53 -0300

    feat: initial commit

---
+++ mobile/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base';
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.950">
+      <Text color="white">SignIn</Text>
+    </Center>
+  );
+}
